5a5cfd31c62ea02813023265485b4629f0f94b2e
betelgeuse06 <mintblue2006@naver.com>
2018-02-23 17:37:17 +0900

    sacend project.c++

---
+++ c++/StringlO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  char name[100];
+  char lang[200];
+
+  std::cout << "what your name" << ;
+  std::cin >> name;
+
+  std::cout << "what your favorite programing languege? " << ;
+  std::cout << "my favorite programing languege is" <<lang<< << '\n';
+  return 0;
+}
